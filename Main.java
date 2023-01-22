7b86b6788982a8832151e1fc0de6bfd6bbb78b63
svelout <64208856+svelout@users.noreply.github.com>
2023-01-22 23:26:39 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,36 @@
+package train;
+
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введи число от: 200 до 1000");
+        int city = sc.nextInt();
+        char a = 64;
+        char b = 64;
+        int j;
+        int c = 25;
+        for(j = 0;j<=25;j++) {
+            a++;
+            b++;
+            for(int x=0;x<=25;x++) {
+                if (x == j) System.out.print(a);
+                else if (x == c) System.out.print(b);
+                else System.out.print(" ");
+            }
+            c--;
+            System.out.println();
+            try {
+                Thread.sleep(city);
+            } catch(InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        System.out.print(dtf.format(now));
+    }
+}
